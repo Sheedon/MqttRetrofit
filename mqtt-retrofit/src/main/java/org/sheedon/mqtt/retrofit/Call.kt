458c0ed47567b1a1a063a4254df0bae26214b841
sheedon <sheedonsun@163.com>
2022-05-12 00:18:35 +0800

    添加注释

--- mqtt-retrofit/src/main/java/org/sheedon/mqtt/retrofit/Call.kt
+++ mqtt-retrofit/src/main/java/org/sheedon/mqtt/retrofit/Call.kt
@@ -7,7 +7,7 @@ import org.sheedon.mqtt.Request
  * 每个调用都会产生自己的请求和响应对。
  * 这可用于实现轮询或重试失败的呼叫。(暂未使用)
  * 调用可以与{@link #publish()}无反馈执行，
- * 也可以与{@link * #enqueue}异步有反馈执行。
+ * 也可以与{@link #enqueue}异步有反馈执行。
  * 无论哪种情况，都可以使用{@link #cancel}随时取消通话。
  *
  * @Author: sheedon
@@ -17,52 +17,109 @@ import org.sheedon.mqtt.Request
 interface Call<T> {
 
     /**
-     * Returns the original request that initiated this call.
+     * Send the request asynchronously, regardless of whether the request was sent successfully.
      */
-    fun request(): Request
-
     fun publish()
 
+    /**
+     * Asynchronously send the request and notify {@code callback} of its response or if an error
+     * occurred talking to the server, creating the request, or processing the response.
+     */
     fun enqueue(callback: Callback<T>?)
 
     /**
-     * Cancels the request, if possible. Requests that are already complete cannot be canceled.
+     * Returns true if this call has been either {@linkplain #publish() published} or {@linkplain
+     * #enqueue(Callback) enqueued}. It is an error to publish or enqueue a call more than once.
+     */
+    fun isExecuted(): Boolean
+
+    /**
+     * Cancel this call. An attempt will be made to cancel in-flight calls, and if the call has not
+     * yet been executed it never will be.
      */
     fun cancel()
 
-    fun isExecuted(): Boolean
-
+    /** True if {@link #cancel()} was called. */
     fun isCanceled(): Boolean
+
+    /**
+     * Returns the original request that initiated this call.
+     */
+    fun request(): Request
 }
 
 /**
  * 对Retrofit方法的调用，该方法将请求发送到串口并返回响应。
  * 每个调用都会产生自己的请求和响应对。
  * 这可用于实现轮询或重试失败的呼叫。(暂未使用)
- * 调用可以与{@link #subscribe}添加订阅，
+ * 调用{@link #enqueue}添加订阅，
+ * 订阅方式有四种,
+ * 1.{@link enqueue},无响应的订阅。
+ * 2.{@link enqueue(consumer)},订阅主题，并且接收响应结果和发生错误。
+ * 3.{@link enqueue(subscribe)},订阅主题，并且接收订阅结果和发送错误，不监听响应结果。
+ * 4.{@link enqueue(fullConsumer)},订阅主题，并且即接收订阅结果，也接收响应结果和发生错误。
+ *
+ * 调用{@link #unsubscribe}取消订阅
+ *
  * 无论哪种情况，都可以使用{@link #cancel}随时取消订阅。
  * */
 interface Observable<T> {
+
+    /**
+     * Send the subscribe asynchronously, regardless of whether the subscribe was sent successfully.
+     */
+    fun enqueue()
+
+    /**
+     * Asynchronously send the subscribe and notify {@code consumer} of its response or if an error
+     * occurred publish and subscribe to the mqtt-server, creating the subscribe/request,
+     * or processing the response.
+     */
+    fun enqueue(consumer: Consumer<T>)
+
+    /**
+     * Asynchronously send the subscribe and notify {@code Subscribe} of its response or if an error
+     * occurred subscribe to the mqtt-server, creating the subscribe/request, or processing the response.
+     */
+    fun enqueue(subscribe: Subscribe<T>)
+
+    /**
+     * Asynchronously send the request and notify {@code callback} of its response or if an error
+     * occurred publish and subscribe to the server, creating the subscribe/request, or processing
+     * the response.
+     */
+    fun enqueue(fullConsumer: FullConsumer<T>)
+
+    /**
+     * Asynchronously send the request and notify {@code callback} of its response or if an error
+     * occurred unsubscribe topic to the mqtt-server, creating the subscribe/request, or processing
+     * the response.
+     */
+    fun unsubscribe(callback: Subscribe<T>?)
+
+    /**
+     * Returns true if this call has been either {@linkplain #enqueue()/#enqueue(Consumer)
+     * /#enqueue(Subscribe)/#enqueue(FullConsumer) enqueued} or {@linkplain #unsubscribe(Subscribe)
+     * unsubscribed}. It is an error to enqueue or unsubscribe a call more than once.
+     */
+    fun isExecuted(): Boolean
+
+    /**
+     * Cancel this subscribe/unsubscribe. An attempt will be made to cancel in-flight subscribe,
+     * and if the call has not yet been executed/unsubscribed it never will be.
+     */
+    fun cancel()
+
+    /** True if {@link #cancel()} was called. */
+    fun isCanceled(): Boolean
+
     /**
      * Returns the original request that initiated this call.
      */
     fun request(): Request
-    fun subscribe(): org.sheedon.mqtt.Subscribe
-
-    fun enqueue()
-    fun enqueue(consumer: Consumer<T>)
-    fun enqueue(consumer: Subscribe<T>)
-    fun enqueue(consumer: FullConsumer<T>)
-
-
-    fun unsubscribe(callback: Subscribe<T>?)
 
     /**
-     * Cancels the request, if possible. Requests that are already complete cannot be canceled.
+     * Returns the original subscribe that initiated this call.
      */
-    fun cancel()
-
-    fun isExecuted(): Boolean
-
-    fun isCanceled(): Boolean
+    fun subscribe(): org.sheedon.mqtt.Subscribe
 }
